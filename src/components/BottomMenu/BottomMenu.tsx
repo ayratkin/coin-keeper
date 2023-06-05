b88906c5a0e906daf8cb0901414f3229c1911003
Ayratkin <ayrat.klim1@yandex.ru>
2023-06-05 21:54:46 +0300

    feat(bottom menu): add bottom menu component

---
+++ src/components/BottomMenu/BottomMenu.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import Styles from './Styles';
+
+const BottomMenu: React.FC = () => {
+  return (
+    <View style={Styles.bottomMenu}>
+      <TouchableOpacity style={Styles.menuButton}>
+        <View style={Styles.menuItem}>
+          <Image
+            style={Styles.menuItemIcon}
+            source={require('../../../assets/img/homepage.png')}
+          />
+          <Text style={Styles.menuItemText}>Главная</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={Styles.menuButton}>
+        <View style={Styles.menuItem}>
+          <Image
+            style={Styles.menuItemIcon}
+            source={require('../../../assets/img/notebook.png')}
+          />
+          <Text style={Styles.menuItemText}>История</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={Styles.menuButton}>
+        <View style={Styles.menuItem}>
+          <Image
+            style={Styles.menuItemIcon}
+            source={require('../../../assets/img/pie-chart.png')}
+          />
+          <Text style={Styles.menuItemText}>Аналитика</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={Styles.menuButton}>
+        <View style={Styles.menuItem}>
+          <Image
+            style={Styles.menuItemIcon}
+            source={require('../../../assets/img/male-avatar.png')}
+          />
+          <Text style={Styles.menuItemText}>Профиль</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BottomMenu;
