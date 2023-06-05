00a59d7d4185e68b82ac2fb3acce1aae63627854
Ayratkin <ayrat.klim1@yandex.ru>
2023-06-05 21:53:01 +0300

    feat(styles): add base styles and variables

---
+++ src/screens/MainScreen/Styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../styles/variables';
+
+const Styles = StyleSheet.create({
+  mainScreenWrapper: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    backgroundColor: colors.lightCoffe,
+  },
+});
+
+export default Styles;
---
+++ src/styles/variables.js
@@ -0,0 +1,13 @@
+export const colors = {
+  red: '#E66371',
+  yellow: '#F9BD7E',
+
+  blue: '#F9BD7E',
+  lightBlue: '#EEF1FF',
+
+  coffe: '#FCDBB6',
+  lightCoffe: '#FFFAEE',
+
+  gray: '#6D6D6D',
+  black: '#000000',
+};
