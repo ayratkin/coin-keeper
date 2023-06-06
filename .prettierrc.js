c45451eff6d554064a7f383983778317e2554e4c
Ayratkin <ayrat.klim1@yandex.ru>
2023-06-06 23:33:24 +0300

    feat(import sorts): add test version for sort imports

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,4 +7,16 @@ module.exports = {
   semi: true,
   endOfLine: 'auto',
   trailingComma: 'all',
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+
+    '^@(redux|constants)/(.*)',
+    '^@configs/(.*)$',
+
+    '^@img/(.*)$',
+    '^@(containers|components|scenes)/(.*)$',
+    '^@styles/(.*)$',
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
 };
