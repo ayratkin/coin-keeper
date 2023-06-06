43b3de5ce6f45ed939719f786576cb2418d4a24d
Ayratkin <ayrat.klim1@yandex.ru>
2023-06-06 23:32:35 +0300

    feat(absolute paths): add and config absolute path libraries

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@styles': './src/styles',
+          '@configs': './src/configs',
+          '@helpers': './src/helpers',
+          '@scenes': './src/scenes',
+          '@containers': './src/containers',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@img': './src/img',
+          '@redux': './src/redux',
+        },
+      },
+    ],
+  ],
 };
