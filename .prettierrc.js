f1308baad0d72ba47aac48d6f1e02f3a8bda4e8c
Ayratkin <ayrat.klim1@yandex.ru>
2023-07-09 18:29:11 +0300

    feat(.prettierrc): add sort rule for styles

--- .prettierrc.js
+++ .prettierrc.js
@@ -16,6 +16,7 @@ module.exports = {
     '^@img/(.*)$',
     '^@(containers|components|scenes)/(.*)$',
     '^@styles/(.*)$',
+    '^[./styles]',
   ],
   importOrderSeparation: false,
   importOrderSortSpecifiers: true,
