5f22e3e3dbf005745d86ff2bf3adaa89c32b8635
Ayratkin <ayrat.klim1@yandex.ru>
2023-07-09 18:36:36 +0300

    feat(variables): add variables for fontSize

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,7 +2,7 @@ export const colors = {
   red: '#E66371',
   yellow: '#F9BD7E',
 
-  blue: '#F9BD7E',
+  blue: '#809DEB',
   lightBlue: '#EEF1FF',
 
   coffe: '#FCDBB6',
@@ -10,4 +10,10 @@ export const colors = {
 
   gray: '#6D6D6D',
   black: '#000000',
+};
+
+export const fontSizes = {
+  main: 12,
+  title: 17,
+  extra: 22,
 };
